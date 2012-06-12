95bf29a86666ad7289b3864e7b927b53c0f718cc
Your Name <mark.huberty@gmail.com>
2012-06-12 15:04:27 -0700

    Added ngram matrix generation code to the disambiguation module. Added test / demonstration code for the functions to the test file.

--- code/psDisambig.py
+++ code/psDisambig.py
@@ -30,3 +30,82 @@
 ## of the authors and should not be interpreted as representing official policies, 
 ## either expressed or implied, of the FreeBSD Project.
 ############################
+
+import string
+import scipy.sparse
+import numpy as np
+import re
+
+def build_ngram_dict(string_list, n=1):
+    """
+    Given a list of strings and a desired n-gram,
+    builds an ngram dictionary of unique ngrams in the entire list
+    Args:
+      string_list: a list of strings to be parsed into ngrams
+      param n:           an integer specifying the n-gram to use
+    Returns:
+      A list of unique ngrams in the string list
+    """
+    ngrams = []
+    for s in string_list:
+        for i in range((len(s) - n)):
+            this_ngram = ''.join(s[j] for j in range(i, i + n))
+            if this_ngram not in ngrams:
+                ngrams.append(this_ngram)
+    return ngrams
+
+def build_ngram_freq(string_list, ngram_dict):
+    """
+    Given an ngram dictionary and a list of strings, returns the ngram frequency
+    distribution for each ngram in each string
+
+    Args:
+      string_list:   a list of strings to be parsed into ngram frequencies
+      ngram_dict:    a dictionary of unique ngrams to use in parsing the strings
+    Returns:
+      A list of dicts, one per string in string_list, with keys
+      as ngrams and values as ngram frequencies in that string.
+    """
+    frequency_list = []
+    for s in string_list:
+        s_freq_dict = {}
+        for n in ngram_dict:
+            n_freq = len(re.findall(n, s))
+            s_freq_dict[n] = n_freq
+        frequency_list.append(s_freq_dict)
+    return frequency_list
+
+def build_ngram_mat(string_list, n=1):
+    """
+    Given a string list, build a sparse term-frequency matrix of ngram frequencies
+    in each string
+
+    Args:
+      string_list:  a list of strings
+      n:            an integer value indicating the ngram to use
+    Returns:
+      A scipy CSR matrix containing the ngram frequency vectors,
+      with rows representing strings and columns representing ngrams
+    """
+    ngram_dictionary = build_ngram_dict(string_list, n)
+    ngram_frequency = build_ngram_freq(string_list, ngram_dictionary)
+    row_idx = []
+    col_idx = []
+    val = []
+    for i, f in enumerate(ngram_frequency):
+        row_idx.extend( [i] * len(f) )
+        col_idx.extend( [ngram_dictionary.index(f_val) for f_val in f] )
+        val.extend( [f[f_val] for f_val in f] )
+    mat = scipy.sparse.csc_matrix((np.array(val),
+                                   (np.array(row_idx),
+                                    np.array(col_idx)
+                                    )
+                                   )
+                                  )
+    out = {'tf_matrix': mat,
+           'ngram_dict': ngram_dictionary
+           }
+    return out
+
+
+
---
+++ code/testDisambig.py
@@ -0,0 +1,15 @@
+import sys
+sys.path.append('/home/markhuberty/Documents/Research/Papers/psClean/code')
+import psDisambig
+
+## Demonstration code for ngram matrix generation
+sample_strings = ['this is a string',
+                  'my dog has fleas',
+                  'we should really use actual data'
+                  ]
+
+test_unigram_mat = psDisambig.build_ngram_mat(sample_strings, n=1)
+test_bigram_mat = psDisambig.build_ngram_mat(sample_strings, n=2)
+
+test_unigram_mat['tf_matrix'].todense()
+test_bigram_mat['tf_matrix'].todense()
