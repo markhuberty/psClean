30b8cea9aaf3c6da092ed7e49e819d3c3ffb9631
Mark Huberty <mark.huberty@gmail.com>
2012-07-18 15:10:03 -0700

    Committing a running version of comprehensive name cleaning to the ./test/ directory

--- test/generate_cleaned_names.py
+++ test/generate_cleaned_names.py
@@ -3,15 +3,18 @@ sys.path.append('/home/markhuberty/Documents/psClean/code')
 import psCleanup
 import psDisambig
 import MySQLdb
+import MySQLdb.cursors
 import re
-import time
 import string
 import numpy as np
 import scipy.sparse as sp
+import csv
+import time
 
 ## Define the cleaning dicts
 all_dicts = [psCleanup.convert_html,
              psCleanup.convert_sgml,
+             psCleanup.clean_symbols,
              psCleanup.concatenators,
              psCleanup.single_space,
              psCleanup.ampersand,
@@ -22,11 +25,25 @@ all_dicts = [psCleanup.convert_html,
 ## Wrap the clean sequence in a useful function
 def clean_wrapper(name_string, dict_list):
     out = psCleanup.rem_diacritics(name_string)
-    out = psCleanup.rem_trail_spaces_out)
     out = psCleanup.stdize_case(out)
-    out = psCleanup.master_clean_dicts([name], all_dicts)
+    out = psCleanup.master_clean_dicts([out], all_dicts)
     out = out[0].strip()
     return(out)
+
+initial_query = """
+SELECT person_id, person_name from (tls201_appln INNER JOIN
+tls207_pers_appln USING(appln_id)) INNER JOIN
+tls206_person USING(person_id)
+WHERE YEAR(appln_filing_date) >= 1980
+"""
+
+subsequent_query = """
+SELECT person_id, person_name from (tls201_appln INNER JOIN
+tls207_pers_appln USING(appln_id)) INNER JOIN
+tls206_person USING(person_id)
+WHERE YEAR(appln_filing_date) >= 1980
+AND person_id > 
+"""
 
 ## Load the sql query as an iterator to save on memory
 conn = MySQLdb.connect(host="127.0.0.1",
@@ -40,24 +57,81 @@ conn = MySQLdb.connect(host="127.0.0.1",
                        )
 
 conn_cursor = conn.cursor()
-conn_cursor.execute("""
-SELECT person_id, person_name FROM tls206_person
-""")
+conn_cursor.execute(initial_query)
 
 ## Open the connection and write each id + name combination to
 ## the file as it's cleaned.
-output_conn = open('', 'wt')
-writer = csv.writer(conn)
-for row in conn_cursor:
-    person_id, person_name = row
-    if len(person_name) > 1:
-        person_name = unicode(person_name)
-    else:
-        continue
-    clean_person_name = clean_wrapper(person_name, all_dicts)
-    clean_person_name = psCleanup.encoder(clean_person_name
-    writer.writerow([person_id, clean_person_name])
-output_conn.close()
+# with open('../data/full_cleaned_name_list.csv', 'wt') as output_conn:
+#     writer = csv.writer(output_conn)
+#     total_time = 0
+#     N = 0
+#     max_id = 0
+#     for row in conn_cursor:
+#         t0 = time.time()
+#         person_id, person_name = row
+#         if len(person_name) > 1:
+#             person_name = unicode(person_name)
+#         else:
+#             continue
+#         clean_person_name = clean_wrapper(person_name, all_dicts)
+#         clean_person_name = psCleanup.encoder(clean_person_name)
+#         writer.writerow([person_id, clean_person_name])
+#         t1 = time.time()
+#         time_diff = t1 - t0
+#         total_time += time_diff
+#         N += 1
+#         if N % 1000 == 0:
+#             print N, total_time, total_time / N
+
+with open('../data/full_cleaned_name_list.csv', 'wt') as output_conn:
+    writer = csv.writer(output_conn)
+    total_time = 0
+    N = 0
+    pid_range = []
+    max_ids = 25000
+    while True:
+        try:
+            row = conn_cursor.fetchone()
+        except MySQLdb.OperationalError:
+            ## DB connection lost, re-set it
+            ## with maxid
+            conn.close()
+            conn = MySQLdb.connect(host="127.0.0.1",
+                                   port=3306,
+                                   user="markhuberty",
+                                   passwd="patstat_huberty",
+                                   db="patstatOct2011",
+                                   use_unicode=True,
+                                   charset='utf8',
+                                   cursorclass = MySQLdb.cursors.SSCursor
+                                   )
+            this_query = subsequent_query + str(person_id)
+            print 're-establishing connection'
+            print this_query
+            conn_cursor = conn.cursor()
+            conn_cursor.execute(this_query)
+            continue
+        t0 = time.time()
+        person_id, person_name = row
+        ## Check the name validity and make sure that
+        ## we haven't seen the person_id before
+        if len(person_name) > 1 and person_id not in pid_range:
+            person_name = unicode(person_name)
+            pid_range.append(person_id)
+            if len(pid_range) > max_ids:
+                pid_range.pop(0)
+        else:
+            continue
+        clean_person_name = clean_wrapper(person_name, all_dicts)
+        clean_person_name = psCleanup.encoder(clean_person_name)
+        writer.writerow([person_id, clean_person_name])
+        t1 = time.time()
+        time_diff = t1 - t0
+        total_time += time_diff
+        N += 1
+        if N % 1000 == 0:
+            print N, total_time, total_time / N
+
 
 conn_cursor.close()
 conn.close()
--- test/test_blocking_strategies.py
+++ test/test_blocking_strategies.py
@@ -8,7 +8,7 @@ import time
 import string
 import numpy as np
 import scipy.sparse as sp
-
+import csv
 
 
 ## Load and clean some names
@@ -61,7 +61,7 @@ clean_names = [psCleanup.rem_trail_spaces(n) for n in clean_names]
 clean_names = [psCleanup.stdize_case(n) for n in clean_names]
 clean_names = [translate_non_alphanumerics(n) for n in clean_names]
 clean_names = psCleanup.master_clean_dicts(clean_names, all_dicts)
-clean_names = [n.strip() for n in clean_names]
+clean_names = [re.sub(' ', '', n) for n in clean_names]
 t1 = time.time()
 
 
@@ -71,16 +71,20 @@ t1 = time.time()
 def block_by_2_ngrams(name_string, ngram_length=2):
     block_dict = {}
     for name in name_string:
-        these_ngrams = set([''.join(name[j] for j in range(i, i + n))
-                            for i in range((len(name) - n))]
+        these_ngrams = set([''.join(name[j] for j in range(i, i + ngram_length))
+                            for i in range((len(name) - ngram_length))]
                            )
-        ngram_combos = get_combinatorial_pairs(these_ngrams)
+        ngram_combos = get_combinatorial_pairs(list(these_ngrams))
         for ngram in ngram_combos:
             if ngram in block_dict:
                 block_dict[ngram].append(name)
             else:
                 block_dict[ngram] = [name]
     return block_dict
+
+
+
+    
 
 
 def get_combinatorial_pairs(ngram_list):
@@ -95,6 +99,54 @@ def get_combinatorial_pairs(ngram_list):
 
 ## Block and count
 
-ngram_blocks = block_by_2_ngrams(clean_names, ngram_length=2)
+#ngram_blocks = block_by_2_ngrams(clean_names, ngram_length=3)
+ngram_blocks = non_overlapping_ngram_block(clean_names, ngram_length=3)
+ngram_overlapping_blocks = block_by_2_ngrams(clean_names, ngram_length=2)
 
-counts = [len(v) for k, v in ngram_blocks]
+non_overlapping_counts = [len(v) for k, v in ngram_blocks.iteritems()]
+overlapping_counts = [len(v) for k, v in ngram_overlapping_blocks.iteritems()]
+
+
+with open('../data/double_bigram_count.csv', 'wt') as f:
+    writer = csv.writer(f)
+    for label, count in zip(ngram_blocks.keys(), counts):
+        writer.writerow([label, count])
+
+## Check the block size on country
+
+conn = MySQLdb.connect(host="127.0.0.1",
+                       port=3306,
+                       user="markhuberty",
+                       passwd="patstat_huberty",
+                       db="patstatOct2011",
+                       use_unicode=True,
+                       charset='utf8'
+                       )
+
+conn_cursor = conn.cursor()
+conn_cursor.execute("""
+SELECT person_name, person_ctry_code FROM tls206_person LIMIT 1000000
+""")
+
+person_vec = conn_cursor.fetchall()
+conn_cursor.close()
+conn.close()
+
+
+names = []
+countries = []
+for p in person_vec:
+    if len(p[0]) > 1:
+        names.append(unicode(p[0]))
+        countries.append(p[1])
+del person_vec
+
+## Doesn't seem to complete in finite time
+#multiblock_dict = psDisambig.multiblock_ngram(clean_names, countries, leading_n=3)
+
+block_keys = []
+multiblock_counts = []
+for c in multiblock_dict.keys():
+    for k in multiblock_dict[c]:
+        block_keys.append((c,k))
+        multiblock_counts.append(len(multiblock_dict[c][k]))
