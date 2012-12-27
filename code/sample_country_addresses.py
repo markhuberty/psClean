02b870b1e013dc7c317d64e944a3f6be39a4d690
Mark Huberty <mark.huberty@gmail.com>
2012-12-26 17:24:48 -0800

    Committing working changes to the random file sampilng code

--- code/sample_country_addresses.py
+++ code/sample_country_addresses.py
@@ -1,4 +1,5 @@
 import csv
+import os
 import random
 
 
@@ -6,19 +7,26 @@ import random
 def resevoir_sample(file_dictreader, n_lines):
     output = [0] * n_lines
     line_count = 0
-    for row in file_dictreader:
+    country = None
+    for row_idx, row in enumerate(file_dictreader):
+        #print row.keys()
         line_count += 1
+        if row['person_ctry_code'] == 'person_ctry_code':
+            continue
+        elif country is None:
+            country = row['person_ctry_code']
+            print country
         addr = row['person_address']
-        if addr is not null and addr !='':
+        if addr is not None and addr !='':
             if line_count < n_lines:
                 output[line_count] = addr
-            else if random.randint(0, line_count) <= n_lines:
+            elif random.randint(0, line_count) <= n_lines:
                 n_addr = len(output)
-                idx = random.randint(0, n_addr)
+                idx = random.randint(0, n_addr - 1)
                 output[idx] = addr
         else:
             continue
-    return output
+    return country, output
 
 os.chdir('/home/markhuberty/Documents/psClean/')
 
@@ -32,24 +40,41 @@ country_files = [f for f in country_files if 'tsv' in f and ' ' not in f]
 country_list = ['DE', 'US', 'KR', 'FR', 'GB', 'DK', 'IT',
                 'CN', 'CA', 'IN']
 
+fieldnames = ['appln_id',
+              'person_id',
+              'person_name',
+              'person_address',
+              'person_ctry_code',
+              'firm_legal_id',
+              'coauthors',
+              'ipc_code',
+              'year'
+              ]
 
-n_addresses_by_country = 500
+n_addresses_by_country = 200
 
 addresses = {}
 for f in country_files:
-    conn = open(f, 'rt')
-    reader = csv.DictReader(conn)
+    full_path = datadir + '/' + f
+    print full_path
+    with open(full_path, 'rt') as conn:
+        reader = csv.DictReader(conn, fieldnames=fieldnames)
+        this_country, address_list = resevoir_sample(reader,
+                                                     n_addresses_by_country
+                                                     )
+        addresses[this_country] = address_list
 
-    for row in reader:
-        this_country = row['person_ctry_cde']
-        sample_this_country = this_country in country_list
+fieldnames = ['person_ctry_code', 'person_address']
 
-        if sample_this_country:
-            addresses[this_country] = resevoir_sample(reader,
-                                                      n_addresses_by_country
-                                                      )
-        else:
-            break
-        
+output_path = datadir + '/sample_country_addresses.csv'
+with open(output_path, 'wt') as conn:
+    writer = csv.writer(conn)
+    writer.writerow(fieldnames)
+    for k in addresses:
+        for row in addresses[k]:
+            if row != 0:
+                long_row = [k, row]
+                writer.writerow(long_row)
+
     
         
