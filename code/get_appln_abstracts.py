c72fec29deb4e953ccd413459277b16f0bd3d27f
Mark Huberty <mark.huberty@gmail.com>
2013-01-08 17:37:06 -0800

    Commit of working version of the abstract query code for NL

--- code/get_appln_abstracts.py
+++ code/get_appln_abstracts.py
@@ -6,23 +6,29 @@ import pandas.io.sql as psql
 
 os.chdir('/home/markhuberty/Documents/psClean/data')
 
+def field_aggfun(col, delim='**'):
+    return delim.join(col)
 
-df_nl = pd.read_csv('')
+def field_identity(col):
+    return col[0]
+
+df_nl = pd.read_csv('./cleaned_data/cleaned_output_NL.tsv')
 
 appln_ids = df_nl.appln_id.values
-
-in_list = ', '.join(list(map(lambda x: '%s', appln_ids)))
-
-this_query = """SELECT appln_id, appln_abstract FROM tls203
-                WHERE appln_id IN (%s)""" % in_list
+in_list = ', '.join(list(map(str, appln_ids)))
 
 db=MySQLdb.connect(host='localhost', port = 3306, user='markhuberty',
                    passwd='patstat_huberty', db = 'patstatOct2011'
                    )
 
+this_query = """SELECT tls203_appln_abstr.appln_id, tls203_appln_abstr.appln_abstract, tls217_appln_ecla.epo_class_symbol FROM tls203_appln_abstr INNER JOIN tls217_appln_ecla ON tls203_appln_abstr.appln_id=tls217_appln_ecla.appln_id WHERE tls203_appln_abstr.appln_id IN (%s) AND tls217_appln_ecla.epo_class_scheme='EC';""" % (in_list) 
 nl_abstracts = psql.frame_query(this_query, con=db)
-nl_abstracts.columns = ['appln_id', 'appln_abstract']
 
-df_out = merge(df_nl, nl_abstracts, how='inner', on='appln_id')
+nl_abstracts.columns = ['appln_id', 'appln_abstract', 'ecla_class']
+nl_grouped = nl_abstracts[['appln_id', 'ecla_class']].groupby('appln_id')
+nl_agg = nl_grouped['ecla_class'].agg(field_aggfun)
+nl_abstracts.set_index('appln_id', inplace=True)
 
-df_out.to_csv('nl_abstract.csv', index=False)
+nl_out = nl_abstracts[['appln_abstract']].join(nl_agg)
+nl_out.to_csv('nl_abstract.csv', index=True)
+nl_abstracts.to_csv('nl_abstracts_raw.csv', index=True)
