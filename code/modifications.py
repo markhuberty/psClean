58d39d59599ba6970a0f659c216b797b35912849
Amma Serwaah-Panin <amma.serwaah@bruegel.org>
2013-01-30 16:48:48 +0100

    Updated possible modifications (split off multiple names and addresses) to pandas_query_clean.

--- code/modifications.py
+++ code/modifications.py
@@ -1,38 +1,95 @@
 # Amendments to query_clean
-# 29 January 2013
+# 30 January 2013
 
 
+## To clean out multiple names
+# Input: name dataframe
+# Return: dataframe with multiple names removes, split, and stacked at the bottom
 
-# To clean out  the addresses
-# Subset dataframe to only have potential names with address information, strip out address information and attach to 'address' field.
-
-
-start = time.time()
-
-criterion = fung_sub['person_name'].map(lambda x: x.endswith(' NL'))
-has_address = fung_sub[criterion]
-has_address = fung_sub[pandas.isnull(fung_sub['person_address'])]
-has_address_ids = has_address.index.tolist()
-has_address['together'] = [ has_address.person_name.ix[pid].split(',') for pid in ha[criterion]s_address_ids]
-has_address['name'] = [ ''.join(has_address.together.ix[pid][0:-2]) for pid in has_address_ids]
-has_address['address'] = [ has_address.together.ix[pid][-2:] for pid in has_address_ids]
-
-end = time.time()
-runtime = end-start
-print runtime
-
-
-
-# To clean out the names
-# Separate multinames into individuals and 'append' data ?
-
-criterion = fung_sub['person_name'].map(lambda x: len(x)>60)
-multi_names = fung_sub[criterion]
-
-def test_length(multiname):
-    names = multiname.split(',')
-    test_lengths = [len( name.split(' ')) for name in names]
-    if 1 in test_lengths:
-        names = [multiname]
+def do_all(fullframe):
+    """ 
+    Takes in dataframe with name information;
+    splits names for rows with multiple name in person field,
+    appends each name to auxillary information,
+    drops rows with multiple names; appends rows of split names 
     
+    """
+    
+    start = time.time()
+   
+    colnames = ['leuven_id', 'fung_name']
+    criterion = fullframe['person_name'].map(lambda x: len(x)>60)
+    multinames = fullframe[criterion]
+
+    splitnames = get_multi_names(multinames) 
+    
+    name1 = fullframe.drop(multinames.index.tolist())
+    mynames = name1.append(splitnames)
+
+    end = time.time()
+    runtime = end-start
+    print runtime
+
+    return mynames
+
+
+def split_multi_names(multiname):
+    """get list of separated names """
+    names = multiname.split(',')
+    check_lengths = [len( name.split(' ')) for name in names]
+    if 1 in check_lengths:
+        names = [multiname]
     return names
+
+def get_multi_names(myframe):
+    """ get data frame of split names with associated information """
+    all_names = list()    
+    for idx in myframe.index.tolist():
+        dd = dict( [ (col, myframe.get_value(idx, col))
+                     for col in colnames] )
+        names = split_multi_names(myframe.get_value(idx, 'person_name'))
+        data = [ dict( dd.items()+ [('person_name', name)]) for name in names]
+    
+        all_names += data
+    split_names = pandas.DataFrame(all_names)
+    return split_names
+         
+
+
+#To clean out address inforamation - find records where person name has country code 
+#    and there is no information in address field.
+
+def do_addresses(myframe):
+    """ 
+    Takes in dataframe, replaces adress names with cleaned name, 
+    inserts address information to address field.
+    Returns updated dataframe.
+
+    """
+    
+    fung_edit = myframe[['person_name', 'person_address']]
+    fung_stay = myframe[['leuven_name', 'leuven_id', 'leuven_hrm_level', 
+                         'person_ctry_code', 'sector', 'fung_id', 
+                         'fung_name', 'Apr12_Person_id', 'han_id', 
+                         'han_name', 'TRUEfung_id', 'has_fung', 'has_han']]
+
+    start = time.time()
+
+    criterion = myframe['person_name'].map(lambda x: x.endswith(' NL'))
+    has_address = myframe[criterion]
+    has_address = has_address[pandas.isnull(has_address['person_address'])]
+    
+    ids = has_address.index.tolist()
+    together = [ has_address.person_name.ix[pid].split(',') for pid in ids]
+
+
+    names = [ (''.join(thing[0:-2]), thing[-2:]) for thing in together]
+    fung_edit.ix[ids] = names 
+    
+    myframe = pandas.merge(fung_stay, fung_edit, left_index = True, right_index = True)
+
+    end = time.time()
+    runtime = end-start
+
+    print runtime
+    return myframe
