bd66f663c7a5d4f117fb25d5b5abcf566cfa9694
Your Name <mark.huberty@gmail.com>
2013-02-01 10:46:25 -0600

    Intermediate commit of name-sorting version of potential nonmatch error code

--- code/flag_potential_nonmatch_errors.py
+++ code/flag_potential_nonmatch_errors.py
@@ -6,12 +6,15 @@ import time
 def cosine_sim(set1, set2):
     numerator = len(set1 & set2)
     denom = len(set1.union(set2))
-    return numerator / float(denom)
+    if denom > 0:
+        return numerator / float(denom)
+    else:
+        return 0
 
 def pds_block(ids, names, n_chars):
     block_val = [n[0:n_chars] for n in names]
-    df = pd.DataFrame({'idx': ids,
-                       'name': names,
+    df = pd.DataFrame({'idx': ids.values,
+                       'name': names.values,
                        'block': block_val}
                       )
     df_grouped = df.groupby('block')
@@ -44,7 +47,7 @@ def flag_errors(ids, names, threshold):
     sets = [set(n) for n in names]
     set_end = time.time()
     set_duration = (set_end - set_start) / len(names)
-    print 'Set time= %s' % set_duration
+    # print 'Set time= %s' % set_duration
     
     out = []
     counter = 0
@@ -55,16 +58,17 @@ def flag_errors(ids, names, threshold):
             s2 = sets[j]
             name1 = names.values[i]
             name2 = names.values[j]
+
             dist = cosine_sim(s1, s2)
+
             if dist > threshold:
                 out.append((ids.values[i], name1,
                             ids.values[j], name2
                             )
                            )
-            else:
-                continue
+
             counter += 1
-            if counter % 10000 == 0:
+            if (counter % 10000) == 0:
                 print counter
                 this_time = time.time()
                 txn_time = (this_time - start_time) / counter
@@ -129,9 +133,23 @@ fung_sub = fung_orig[bool_idx]
 
 ## Issues here:
 ## 1. What about first-letter misspellings, name reversals, etc
-## 2. Will ID stuff w/ separate names that aren't 
+## 2. Will ID stuff w/ separate names that aren't
+
+## Now resort names
+def sort_name(name_instance, min_length):
+    name_split = name_instance.split()
+    name_sort = sorted([n for n in name_split if len(n) >= min_length ])
+    name_out = ' '.join(name_sort)
+    return name_out
+
+def sort_names(names, min_length=3):
+    out = [sort_name(n, min_length) for n in names]
+    return pd.Series(out)
+
+sorted_names = sort_names(fung_sub.name, min_length=2)
+    
 nl_potential_flags = flag_errors_blocked(fung_sub.unique_id,
-                                         fung_sub.name,
+                                         sorted_names,
                                          3,
                                          0.98,
                                          test_threshold=None
@@ -151,3 +169,114 @@ df.to_csv('nl_candidate_false_nonmatches.csv',
           index=False
           )
 
+
+## Some experiments w/ metaphone hashing
+import fuzzy
+import math
+dhash = fuzzy.DMetaphone(3)
+df_hash = nl_potential_flags[nl_potential_flags.keys()[-1]]
+
+def hash_names(ids, names, hashfun, num_hashes = 3):
+    hashes = []
+    for idx1, n in zip(ids.values, names.values):
+        n_split = [ns for ns in n.split() if len(ns) > 2]
+        hashlist = sorted([hashfun(ns)[0] for ns in n_split])
+        hash_out = [None] * num_hashes
+        for idx2, h in enumerate(hashlist):
+            if idx2 < num_hashes:
+                hash_out[idx2] = h
+            else:
+                break
+        this_hash = [idx1]
+        this_hash.extend(hash_out)
+        hashes.append(this_hash)
+    return hashes
+
+#test_hash = hash_names(fung_sub.unique_id, fung_sub.name, dhash, 3)
+
+def stack_groups(grouplist):
+    for idx, group in enumerate(grouplist):
+        if idx == 0:
+            df = group
+        else:
+            df = df.append(group, ignore_index=True)
+    return df
+
+## Returns the indices only
+def check_flag_dim(df, num_hashes):
+    ct_threshold = math.ceil(num_hashes / float(2.0))
+    grouped = df.groupby(['name1', 'name2'])
+    gsize = grouped.size()
+    #gsize = gsize[gsize >= ct_threshold]
+    return gsize
+
+
+def multihash_flagger(ids, names, hashfun, num_hashes, sim_threshold):
+    name_hashes = hash_names(ids, names, hashfun, num_hashes)
+    colnames = ['idx']
+    hash_colnames = ['hash' + str(num) for num in range(num_hashes)]
+    colnames.extend(hash_colnames)
+    print 'building hash dataframe'
+    name_hashes = pd.DataFrame(name_hashes, columns=colnames)
+    print name_hashes.shape
+    df = pd.DataFrame({'idx': ids, 'name':names})
+    all_groups = []
+    for col in hash_colnames:
+        print 'grouping names'
+        names_grouped = df.groupby(name_hashes[col])
+        print 'flagging errors'
+        flag_groups = []
+        for groupname, group in names_grouped:
+            flags = flag_errors(group.idx, group.name, sim_threshold)
+            if len(flags) > 0:
+                df_flags = pd.DataFrame(flags, columns=['id1', 'name1', 'id2', 'name2'])
+                df_flags['hashname'] = groupname
+                print df_flags.shape
+                flag_groups.append(df_flags)
+            else:
+                continue
+
+        print 'stacking flag groups'
+        print len(flag_groups)
+        stacked_groups = stack_groups(flag_groups)
+        print 'appending stacked groups'
+        all_groups.append(stacked_groups)
+    print 'hashset length'
+    print len(all_groups)
+    for g in all_groups:
+        print g.shape
+    df_all = stack_groups(all_groups)
+    print 'output shape'
+    print(df_all.shape)
+    
+    valid_pairs = check_flag_dim(df_all, num_hashes)
+
+    return valid_pairs, df_all
+
+test_hash, test_df = multihash_flagger(fung_sub.unique_id,
+                                       fung_sub.name,
+                                       dhash,
+                                       5,
+                                       0.95
+                                       )
+
+name_hashes = hash_names(fung_sub.unique_id,
+                         fung_sub.name,
+                         dhash,
+                         3
+                         )
+
+df = pd.DataFrame(name_hashes, columns=['idx', 'h1', 'h2', 'h3'])
+
+df2 = fung_sub[['unique_id', 'name']]
+df2.columns=['idx', 'name']
+grouped = df2.groupby(df['h1'])
+
+out = {}
+for groupname, group in grouped:
+    flags = flag_errors(group.idx, group.name, 0.98)
+    if len(flags) > 0:
+        out[groupname] = flags
+
+
+## What if we just resort the names and then block?
