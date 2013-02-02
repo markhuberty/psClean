2dfad1b6bb274434293299b90c7d4135ea1983e9
Your Name <mark.huberty@gmail.com>
2013-02-02 12:39:20 -0600

    Updated the name cleaning code to incorporate changes elsewhere in the disambig file preparation process

--- code/test_nl_name_clean.py
+++ code/test_nl_name_clean.py
@@ -2,9 +2,31 @@ import pandas as pd
 import time
 import modifications
 
+## Read in the original file
+nl_orig = pd.read_csv('/mnt/db_master/patstat_raw/fleming_inputs/cleaned_output_NL.tsv')
+nl_fung = pd.read_csv('/mnt/db_master/patstat_raw/disambig_input_data/nl_test_data/csv')
 
-nl_data = pd.read_csv('/Users/markhuberty/Documents/Research/Papers/psClean/data/nl_test_data.csv')
+nl_fung = nl_fung[['Unique_Record_ID', 'Lat', 'Lng']]
+
+
+## Read in the file used for the disambig itself
+
+## Subset the disambig file to contain only the unique_id and the
+## lat/long
+
+## Merge them
+
+## Check for > 1 name in the name field
+
+nl_data = do_all(nl_data)
+
+
+## Check for addresses in the address field
+
 
 nl_data = do_addresses(nl_data, 'NL')
 
-nl_data = do_all(nl_data)
+
+## Write out.
+
+
