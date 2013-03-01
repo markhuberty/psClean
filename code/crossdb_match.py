bad854faeeab9c79e9203c705e00e3233b32ee5c
Your Name <mark.huberty@gmail.com>
2013-03-01 15:19:42 -0600

    Added scripts for post-processing and matching the fung output to han and leuven'

--- code/crossdb_match.py
+++ code/crossdb_match.py
@@ -1,73 +1,95 @@
+#!/usr/bin/env
+# -*- coding: utf-8 -*-
+
+"""
+Command-line script for matching the fung output to the HAN
+and Leuven data and writing out the map.
+
+Assumes the following argument order:
+(1) the formatted fung input file (csv)
+(2) the formatted fung output file (csv)
+(3) the han file 
+(4) the leuven file
+
+Assumes the following fields in each file:
+(fung input) unique_id, orig_id, name
+(fung output) unique_id, orig_unique_id
+(han) han_id, person_id, han_name
+(leuven) name, person_id, hrm_l2_id, hrm_level, person_address, person_name
+"""
+
 import os
 import pandas as pd
 import string
+import sys
 
-os.chdir('/home/Bruegel/patstat_compare')
-#os.chdir('/Users/markhuberty/Documents/Research/Papers/psClean/data')
+sys.stdout = os.fdopen(sys.stdout.fileno(), 'wb', 0)
 
+## Get the filenames off the command line
+## NOTE: assumes that the fung outputs have
+## already been processed with postprocess_fung_output.py
 
-#ehp.to_csv('fung_leuven_han_matchedFEB')
-#akzo = ehp[ ehp['fung_name2'] == 'AKZO NOBEL']
-#'nl_test_data_12Feb2013.csv'
-#neth = pandas.read_csv(fung_input)
+filenames = [f for idx, f in enumerate(sys.argv) if idx > 0]
+fung_input_file = filenames[0]
+fung_output_file = filenames[1]
+han_data_file = filenames[2]
+leuven_data_file = filenames[3]
 
+print(fung_input_file,
+      fung_output_file,
+      han_data_file,
+      leuven_data_file
+      )
 
-def disambig_to_list(disambig_row,
-                     delim_primary='###',
-                     delim_secondary=','
-                     ):
-    """
-    Takes one row in the fung disambiguator final output, of form
-    unique_id###match_prob###match_id,values and returns a
-    list of form [(unique_id, match_id), ]
-    """
-    primary_split = disambig_row.split(delim_primary)
-    matching_ids = primary_split[2].rstrip(',\n').split(delim_secondary)
-    input_list = [(int(primary_split[0]), int(mid)) for mid in matching_ids]
+## Load the files
+fung_output = pd.read_csv(fung_output_file)
+fung_input = pd.read_csv(fung_input_file)
+han_data = pd.read_csv(han_data_file)
+leuven_data = pd.read_csv(leuven_data_file)
 
-    return input_list
+print fung_output.shape
+print fung_input.shape
+print han_data.shape
+print leuven_data.shape
 
-def disambig_to_df(disambig_out,
-                   delim_primary='###',
-                   delim_secondary=','
-                   ):
-    """
-    Reformats the fung disambuguator output to a python
-    data frame of form unique_id:match_id
-    
-    """
-    df_input = []
-    with open(disambig_out, 'r') as infile:
-        for row in infile:
-            output_element = disambig_to_list(row,
-                                              delim_primary,
-                                              delim_secondary
-                                              )
-            df_input.extend(output_element)
-    print df_input[0]
-    df = pd.DataFrame(df_input, columns=['unique_id', 'orig_id'])
-    return df
+## Merge the fung inputs and outputs
+fung_map = pd.merge(fung_input,
+                    fung_output,
+                    left_on='unique_id',
+                    right_on='unique_id',
+                    copy=False
+                    )
 
 
-
-predisambig_input = 'nl_test_data_12Feb2013.csv'
-diambig_results = 'nl_test_output.txt'
-
-
-fung = disambig_to_df(disambig_results)
-neth = pd.read_csv(predisambig_input)
-
-# subset because it is quite a large dataframe
-neth = neth[['Person', 'Unique_Record_ID', 'Name']]
-
-fung_map = pd.merge(neth, fung, left_on = 'Unique_Record_ID', right_on = 'orig_id')
-
-#disambigid1, disambigid2 are the ids created in the diambiguation process
-fung_map.columns = ['person_id', 'disambigid1', 'name', 'fung_id', 'disambigid2']
+print 'fung_map diagnostics:'
+print fung_map.shape
+print fung_map.columns
 
 
-leuven11 = pandas.read_csv('leuven2011')
-han11 = pandas.read_csv('han2011')
+leuven_data =  leuven_data[['person_id',
+                            'hrm_l2_id',
+                            'hrm_level',
+                            'person_address',
+                            'person_name'
+                            ]
+                           ]
+leuven_data.columns = ['person_id',
+                       'leuven_id',
+                       'leuven_ld_level',
+                       'leuven_address',
+                       'person_name'
+                       ]
 
-full_map = pandas.merge( leuven11, fung_map, left_on = 'person_id', right_on = 'person')
-full_map = pandas.merge( han11, full_map, left_on = 'OCT11_Person_id', right_on = 'person')
+
+
+han_data = han_data[['HAN_ID','OCT11_Person_id','Person_name_clean']]
+han_data.columns = ['han_id',
+                    'person_id',
+                    'han_name'
+                    ]
+
+
+full_map = pd.merge( leuven_data, fung_map, left_on = 'person_id', right_on = 'person_id', how='outer')
+full_map = pd.merge( han_data, full_map, left_on = 'person_id', right_on = 'person_id', how='outer')
+
+full_map.to_csv('fung_han_leuven_map.csv', index=False)
