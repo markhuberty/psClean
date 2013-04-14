a74806982ee71547f4bdcf2f1933ba62a22c7e55
Mark Huberty <mark.huberty@gmail.com>
2013-04-14 09:39:48 -0700

    Refactored the han-leuven training data constructor to mimic the dedupe logic

--- code/generate_leuven_dedupe_training_data.py
+++ code/generate_leuven_dedupe_training_data.py
@@ -1,19 +1,26 @@
 import pandas as pd
 import json
 import numpy as np
+import AsciiDammit
+import re
 
 ## Use the leuven hand-labeled data to generate
 ## training pairs
-df_leuven = pd.read_csv('../data/leuven2011.csv')
+df_leuven = pd.read_csv('../data/patstat_benchmark/leuven2011.csv')
 df_leuven = df_leuven[df_leuven.columns[1:]]
 df_leuven = df_leuven[df_leuven.hrm_level == 1]
 
-df_patstat = pd.read_csv('../data/nl_test_data_5March2013.csv')
+df_patstat = pd.read_csv('../../dedupe/examples/patent_example/patstat_dedupe_input_consolidated.csv')
 
 df_patstat.Name.fillna('', inplace=True)
 df_patstat.Coauthor.fillna('', inplace=True)
 df_patstat.Class.fillna('', inplace=True)
+df_patstat.Lat.fillna('0.0', inplace=True)
+df_patstat.Lng.fillna('0.0', inplace=True)
 
+df_patstat = df_patstat[['Name', 'Coauthor', 'Class', 'Person', 'Lat', 'Lng']]
+
+df_patstat.to_csv('../data/patstat_dedupe_input.csv', index=False)
 ## Group the records by unique ID
 grouped = df_leuven.groupby('hrm_l2_id')
 
@@ -24,19 +31,53 @@ id_sizes = grouped.size()
 clumped = id_sizes[id_sizes > 1]
 individual = id_sizes[id_sizes == 1]
 
+def preProcess(column):
+    """
+    Do a little bit of data cleaning with the help of
+    [AsciiDammit](https://github.com/tnajdek/ASCII--Dammit) and
+    Regex. Things like casing, extra spaces, quotes and new lines can
+    be ignored.
+    """
+
+    column = AsciiDammit.asciiDammit(column)
+    column = re.sub('  +', ' ', column)
+    column = re.sub('\n', ' ', column)
+    column = column.strip().strip('"').strip("'").lower().strip()
+    return column
+
+def gen_frozenset_dict(val_str, delim='**'):
+    val_iter = val_str.split(delim)
+    val_iter = [preProcess(v) for v in val_iter]
+    d = {'__class__': 'frozenset', '__value__':val_iter}
+    return d
+
 def generate_matching_dicts(df_pair):
     dict_pairs = []
     for i, idx1 in enumerate(df_pair.index):
         for idx2 in df_pair.index[(i + 1):]:
             item1 = df_pair.ix[idx1].to_dict()
-            #item1['LatLong'] = (item1['Lat'], item1['Lng'])
-            item1['LatLong'] = str(item1['Lat']) + '**' + str(item1['Lng'])
+
+            item1['Name'] = preProcess(item1['Name'])
+            
+
+            item1['LatLong'] = [item1['Lat'], item1['Lng']]
+            class_dict = gen_frozenset_dict(item1['Class'])
+            item1['Class'] = class_dict
+            author_dict = gen_frozenset_dict(item1['Coauthor'])
+            item1['Coauthor'] = author_dict
             item1 = cast_serializable_ints(item1)
+            
             item2 = df_pair.ix[idx2].to_dict()
             #item2['LatLong'] = (item1['Lat'], item2['Lng'])
-            item2['LatLong'] = str(item2['Lat']) + '**' + str(item2['Lng'])
+            item2['LatLong'] = [float(item2['Lat']), float(item2['Lng'])]
             del item1['Lat'], item1['Lng'], item2['Lat'], item2['Lng']
+            item2['Name'] = preProcess(item2['Name'])
+            class_dict = gen_frozenset_dict(item2['Class'])
+            item2['Class'] = class_dict
+            coauthor_dict = gen_frozenset_dict(item2['Coauthor'])
+            item2['Coauthor'] = coauthor_dict
             item2 = cast_serializable_ints(item2)
+
             dict_pairs.append([item1, item2])
     return dict_pairs
 
@@ -45,14 +86,26 @@ def generate_nonmatching_dicts(df1, df2):
     for idx1 in df1.index:
         for idx2 in df2.index:
             item1 = df1.ix[idx1].to_dict()
-            item1['LatLong'] = str(item1['Lat']) + '**' + str(item1['Lng'])
+            item1['LatLong'] = [item1['Lat'], item1['Lng']]
+            class_dict = gen_frozenset_dict(item1['Class'])
+            item1['Class'] = class_dict
+            author_dict = gen_frozenset_dict(item1['Coauthor'])
+            item1['Coauthor'] = author_dict
+            item1['Name'] = preProcess(item1['Name'])
+
             # item1['LatLong'] = (item1['Lat'], item1['Lng'])
             item1 = cast_serializable_ints(item1)
             item2 = df2.ix[idx2].to_dict()
-            item2['LatLong'] = str(item2['Lat']) + '**' + str(item2['Lng'])
+            item2['Name'] = preProcess(item2['Name'])
+            item2['LatLong'] = [float(item2['Lat']), float(item2['Lng'])]
+            class_dict = gen_frozenset_dict(item2['Class'])
+            item2['Class'] = class_dict
+            coauthor_dict = gen_frozenset_dict(item2['Coauthor'])
+            item2['Coauthor'] = coauthor_dict
             # item2['LatLong'] = (item1['Lat'], item2['Lng'])
             del item1['Lat'], item1['Lng'], item2['Lat'], item2['Lng']
             item2 = cast_serializable_ints(item2)
+
             dict_pairs.append([item1, item2])
     return dict_pairs
 
@@ -64,8 +117,9 @@ def cast_serializable_ints(d):
 
 
 ## Serialize the clumped data into pairs
+print 'generating matching pairs'
 matching_pairs = []
-max_pairs = 2000
+max_pairs = 50000
 for idx in clumped.index:
     person_ids = df_leuven.person_id[df_leuven.hrm_l2_id == idx]
     orig_data = df_patstat[df_patstat.Person.isin(person_ids)]
@@ -75,6 +129,7 @@ for idx in clumped.index:
     if len(matching_pairs) > max_pairs:
         break
 
+print 'generating nonmatching pairs'
 nonmatching_pairs = []
 for i, idx in enumerate(clumped.index):
     ids_1 = df_leuven.person_id[df_leuven.hrm_l2_id == idx]
@@ -99,6 +154,6 @@ for i, idx in enumerate(clumped.index):
     
 training_dict = {0: nonmatching_pairs, 1:matching_pairs}
 
-with open('../data/patstat_example_training.json', 'wt') as f:
+with open('../data/patstat_training_consolidated.json', 'wt') as f:
     json.dump(training_dict, f)
     
--- code/match_citl_innovators.py
+++ code/match_citl_innovators.py
@@ -1,9 +1,11 @@
+import pandas as pd
 import csv
 from dedupe.affinegap import normalizedAffineGapDistance
 import os
 import sys
 import numpy as np
 import itertools
+import fuzzy
 
 
 
@@ -27,39 +29,60 @@ def format_name(name):
     else:
         return ' '.join(name_sort)
 
-def block_by_name(names, block_len):
-    block_str = [n[0:block_len] for n in names]
+def block_by_name(names, block_len, how='char'):
+    if how == 'char':
+        block_str = [n[0:block_len] for n in names]
+    if how == 'metaphone':
+        dmeta = fuzzy.DMetaphone(block_len)
+        block_str = [dmeta(n)[0] for n in names]
+        
     return block_str
 
+innovator_names = [format_name(n) for n in innovators['name'].values]
+emitter_names = [format_name(n) for n in emitters['name'].values]
+innovators['name'] = innovator_names
+emitters['name'] = emitter_names
 
-innovators['name'] = [format_name(n) for n in innovators['name']]
-emitters['name'] = [forname_name(n) for n in innovators['name']]
+innovator_block = block_by_name(innovators.name, 3, how='metaphone')
+emitter_block = block_by_name(emitters.name, 3, how='metaphone')
 
-innovator_block = block_by_name(innovators.name, 3)
-emitters_block = block_by_name(emitters.name, 3)
-
+innovators['block'] = innovator_block
+emitters['block'] = emitter_block
+innovators.set_index('block', inplace=True)
+emitters.set_index('block', inplace=True)
 ## Take the set intersection; note that this means we don't examine some names...
-block_intersect = set(innovators_block).intersection(set(emitters_block))
-block_union = set(innovators_block).union(set(emitters_block))
+block_intersect = set(innovator_block).intersection(set(emitter_block))
+block_union = set(innovator_block).union(set(emitter_block))
 
 
-## Walk across the blocks and match
-
-## First group data by block
-grouped_innovators = innovators.groupby(innovators_block)
-grouped_emitters = emitters.groupby(emitters_block)
 
 ## then compare
+def return_match_index(query, possible):
+    dist = [normalizedAffineGapDistance(query, p)
+            for p in possible
+            ]
+    match_index = np.argmin(dist)
+    return match_index, dist[match_index]
+
 out = []
 for idx, block in enumerate(block_intersect):
-    if block in innovator_block:
-        print 'Disambiguating block ' + str(idx) + ' of ' + str(len(blocks))
-        for id, emitter in itertools.izip(grouped_emitters.groups[block].id, grouped_emitters.groups[block].name):
-            dist = [normalizedAffineGapDistance(emitter, innovator) for innovator in grouped_innovators.groups[block].name]
-            match_index = np.argmin(dist)
-            match_name = grouped_innovators.groups[block].name[match_index]
-            match_idx = grouped_innovators.groups[block].id[match_index]
-            match_pair = (id, emitter, match_idx, match_name, dist)
+    print 'Disambiguating block ' + str(idx) + ' of ' + str(len(block_intersect))
+    emitter_block = emitters.ix[block]
+    innovator_block = innovators.ix[block]
+    if len(emitter_block.shape) == 1:
+        emitter = emitter_block['name']
+        emitter_id = emitter_block['id']
+        match_index, match_dist = return_match_index(emitter, innovator_block['name'])
+        match_name = innovators.ix[block]['name'][match_index]
+        match_idx = innovators.ix[block]['id'][match_index]
+        match_pair = (emitter['id'], emitter, match_idx, match_name, match_dist)
+        out.append(match_pair)
+    else:
+        for id, emitter in emitters.ix[block].iterrows():
+            match_index, match_dist = return_match_index(emitter['name'], innovator_block['name'])
+            match_name = innovators.ix[block]['name'][match_index]
+            match_idx = innovators.ix[block]['id'][match_index]
+            match_pair = (emitter['id'], emitter['name'], match_idx, match_name, match_dist)
             out.append(match_pair)
 
 df_out = pd.DataFrame(out, columns=['emitter_id', 'emitter_name', 'innovator_id', 'innovator_name', 'dist'])
