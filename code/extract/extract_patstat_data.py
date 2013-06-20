39fb8ace697c535784dc7df8ab228998333f2775
Mark Huberty <markhuberty@mill.(none)>
2013-06-20 14:54:34 -0400

    Moved the extract code to its own directory for clarity

--- code/extract/extract_patstat_data.py
+++ code/extract/extract_patstat_data.py
@@ -1,51 +1,25 @@
 ############################
 ## Author: Mark Huberty, Mimi Tam, and Georg Zachmann
 ## Date Begun: 13 November 2012
-## Purpose: Module to clean inventor / assignee data in the PATSTAT patent
-##          database
-## License: BSD Simplified
-## Copyright (c) 2012, Authors
+## Purpose: Extract PATSTAT inventor data and do preliminary cleaning
+## License: See LICENSE file in root
+## Copyright (c) 2012-2013, Authors
 ## All rights reserved.
-##
-## Redistribution and use in source and binary forms, with or without
-## modification, are permitted provided that the following conditions are met:
-## 
-## 1. Redistributions of source code must retain the above copyright notice, this
-##    list of conditions and the following disclaimer. 
-## 2. Redistributions in binary form must reproduce the above copyright notice,
-##    this list of conditions and the following disclaimer in the documentation
-##    and/or other materials provided with the distribution. 
-## 
-## THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-## ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-## WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-## DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
-## ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-## (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-## LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-## ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-## (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-## SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-## 
-## The views and conclusions contained in the software and documentation are those
-## of the authors and should not be interpreted as representing official policies, 
-## either expressed or implied, of the FreeBSD Project.
 ############################
-import os
-import subprocess
-import csv
-import psCleanup
-import MySQLdb
-import time
-import numpy
-import pandas as pd
-import pandas.io.sql as psql
 from IPython.parallel import Client
-import gc
-import itertools as it
 from cleanup_dicts import *
 from psCleanup import name_address_dict_list, coauth_dict_list, legal_regex
+import MySQLdb
+import csv
+import gc
+import itertools as it
+import numpy
+import os
+import pandas as pd
+import pandas.io.sql as psql
+import psCleanup
 import re
+import time
 
 # @dview.parallel(block=True)
 # def ipc_clean_wrapper(ipc_code_list):
@@ -56,36 +30,31 @@ def coauthor_aggfun(col):
      col_short = list(col)[0:9]
      return '**'.join(col_short)
 
-# Set up MySQL connection
-
-db=MySQLdb.connect(host='localhost', port = 3306, user='mimitam',
-                   passwd='tam_patstat2011', db = 'patstatOct2011'
-                   )
-
 def myquery(query, db_connection, colnames):
      output = psql.frame_query(query, con=db_connection)
      output.columns = colnames
      return output
 
+# Set up MySQL connection
+db=MySQLdb.connect(host='localhost', port = 3306, user='mimitam',
+                   passwd='tam_patstat2011', db = 'patstatOct2011'
+                   )
     
 # Years to group patent data by.
- 
-years = [#'1990',
-         '1991',
-         '1992',
-         '1993', '1994', '1995', '1996', '1997', '1998', '1999',
-         '2000', '2001', '2002', '2003', '2004', '2005', '2006',
-         '2007', '2008', '2009',
-         '2010', '2011']
+years = ['1991', '1992', '1993', '1994', '1995', '1996', '1997',
+         '1998', '1999', '2000', '2001', '2002', '2003', '2004',
+         '2005', '2006', '2007', '2008', '2009', '2010', '2011'
+         ]
 
 total_elapsed_time = 0
 
-
+# This loop extract PATSTAT data by year, cleans and aggregates it,
+# and then writes out the data to country-specific files. 
 for year in years:
      # Start the cluster for each run; clears out the memory better
      os.system('ipcluster start -n 2 &')
      time.sleep(60)
-     # subprocess.Popen(['ipcluster', 'start', '-n', '2'], stdout=subprocess.PIPE) 
+
      par_client = Client()
      dview = par_client[:]
      dview.block = True
@@ -159,31 +128,34 @@ for year in years:
      print len(name_output), len(ipc_output)
      
      name_clean_time = time.time()
-     
+
+     # Clean names and separate legal IDs if possible
      names_ids = name_clean_wrapper.map(list(name_output['person_name']))
      dview.results.clear()
      par_client.results.clear()
      name_output['person_name'], name_output['firm_legal_id'] = it.izip(*names_ids)
      del names_ids
      gc.collect()
-     
+
+     # Clean addresses
      name_output['person_address'] = address_clean_wrapper.map(list(name_output['person_address']))
      dview.results.clear()
      par_client.results.clear()
      print time.strftime('%c', time.localtime())
      print 'Names clean'
+     
      # ID the coauthors and join
-
      coauthor_list = []
      coauth_time = time.time()
      names_grouped = name_output.groupby('appln_id')
      coauthors = names_grouped['person_name'].agg(coauthor_aggfun)
      coauthors.name = 'coauthors'
-
+     
      name_output.set_index('appln_id', inplace=True)
      name_output = name_output.join(coauthors)
      print (time.time() - coauth_time)
 
+     # Clean the coauthors and append back to the original file
      coauth_clean_time = time.time()
      coauth_clean = []
      for n,c in it.izip(name_output['person_name'], name_output['coauthors']):
@@ -197,12 +169,10 @@ for year in years:
      name_output['coauthors'] = coauth_clean
      
      print time.time() - coauth_clean_time
-
-     # Then here just join the coauthors to the original db
-    
      print time.strftime('%c', time.localtime())
      print 'Coauthors aggregated'
-    
+
+     # Format the IPC patent codes as a delimited string
      ipc_grouped = ipc_output.groupby('appln_id')
      ipc_cat = ipc_grouped['ipc_code'].agg(coauthor_aggfun)
      name_output = name_output.join(ipc_cat, how='left')
@@ -211,6 +181,7 @@ for year in years:
                                 for ipc in name_output['ipc_code']]
      del ipc_output, ipc_cat
      gc.collect()
+
      # Write out files by country-year
      name_clean_finish = time.time()
      print 'Cleaning time per name + address + ipc code'
@@ -219,7 +190,8 @@ for year in years:
      end_time = time.time()
      elapsed_time = end_time - start_time
      n_records = str(len(name_output))
-     print 'Time elapsed for ' + year + ' and ' + n_records + ' records: ' + str(numpy.round(elapsed_time, 0))
+     print 'Time elapsed for ' + year + ' and ' + \
+           n_records + ' records: ' + str(numpy.round(elapsed_time, 0))
 
      name_output = name_output.reset_index()
      name_output['year'] = year
@@ -229,12 +201,14 @@ for year in years:
           header_bool=True
      else:
           header_bool=False
-          
+
+     # Write out; if file exists, then append
      for country, group in grouped_country:
           print country
           output_filename = 'cleaned_output_' + country + '.tsv'
           group.to_csv(output_filename, mode='a', sep='\t', header=header_bool)
 
+     # Reset the data and stop the cluster
      del name_output
      del grouped_country
      os.system('ipcluster stop &')
